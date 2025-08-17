f7a797cf6d403e269bec59a3ab752e3332ff06fd
Angel Avelar-Bonilla <angelavelarbonilla@gmail.com>
2025-08-17 17:37:03 -0400

    Update theme to Onedark warm

--- .config/nvim/lua/plugins/ui.lua
+++ .config/nvim/lua/plugins/ui.lua
@@ -1,5 +1,14 @@
 return {
   { "nvim-lualine/lualine.nvim", config = true },
-  { "folke/tokyonight.nvim" },
+  {
+    "navarasu/onedark.nvim",
+    priority = 1000,
+    config = function()
+      require('onedark').setup {
+        style = 'warm'
+      }
+      require('onedark').load()
+    end,
+  },
   { "lukas-reineke/indent-blankline.nvim", main = "ibl", opts = {} },
 }
