63f265729f543f175715eeb11cc820ec677062d9
Angel Avelar-Bonilla <angelavelarbonilla@gmail.com>
2025-08-16 23:24:40 -0400

    Move neovim options into seperate file

---
+++ .config/nvim/lua/core/options.lua
@@ -0,0 +1,8 @@
+-- Neovim options
+vim.opt.relativenumber = true
+vim.opt.clipboard = "unnamedplus"
+vim.opt.termguicolors = true
+vim.opt.expandtab = true
+vim.opt.shiftwidth = 2
+vim.opt.smartindent = true
+
