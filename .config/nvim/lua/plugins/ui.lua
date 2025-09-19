3c3500c84274a394696cc337fdf018c19afba720
Angel Avelar-Bonilla <angelavelarbonilla@gmail.com>
2025-09-19 15:06:39 -0400

    Add transparency to nvim, use background img for wezterm

--- .config/nvim/lua/plugins/ui.lua
+++ .config/nvim/lua/plugins/ui.lua
@@ -1,13 +1,17 @@
 return {
   { "nvim-lualine/lualine.nvim", config = true },
+  { "lukas-reineke/indent-blankline.nvim", main = "ibl", opts = {} },
   { 
     "catppuccin/nvim",
     name = "catppuccin",
     priority = 1000,
-    config = function()
-      require("catppuccin").setup({})
-      vim.cmd.colorscheme "catppuccin-mocha"
+    opts = {
+      flavour = "mocha",
+      transparent_background = true,
+    },
+    config = function(_, opts)
+      require("catppuccin").setup(opts)
+      vim.cmd.colorscheme "catppuccin"
     end,
   },
-  { "lukas-reineke/indent-blankline.nvim", main = "ibl", opts = {} },
 }
--- .wezterm.lua
+++ .wezterm.lua
@@ -19,4 +19,6 @@ config.color_scheme = "Catppuccin Mocha"
 
 config.audible_bell = "Disabled"
 
+config.window_background_image = "C:/Users/liasf/Pictures/ow-blurred.png"
+
 return config
