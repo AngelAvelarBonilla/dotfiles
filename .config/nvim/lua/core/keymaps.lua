5a219a05bfad0749c0537c3f7bab793a25c99929
Angel Avelar-Bonilla <angelavelarbonilla@gmail.com>
2025-11-09 19:29:10 -0500

    Rebind 'gg' to 'gg0'

--- .config/nvim/lua/core/keymaps.lua
+++ .config/nvim/lua/core/keymaps.lua
@@ -1,12 +1,13 @@
 -- TODO AAB:
 -- Move Nvimtree bindings into tree file
--- Change bindings for navigating between vim splits
 
 -- Keybindings
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
 
 local map = vim.keymap.set
+
+map("n", "gg", "gg0", { noremap = true, silent = true, desc = "Go to start of file and first column" })
 
 -- NvimTree
 map("n", "<leader>e", ":NvimTreeToggle<CR>", { desc = "Toggle file explorer" })
