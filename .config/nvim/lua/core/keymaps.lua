a37cd9b7bed914b759e511ae31186add67c6ab53
Angel Avelar-Bonilla <angelavelarbonilla@gmail.com>
2025-11-09 19:41:35 -0500

    Remove gg and G remap since I can just do ggVG to highlight whole file

--- .config/nvim/lua/core/keymaps.lua
+++ .config/nvim/lua/core/keymaps.lua
@@ -7,9 +7,6 @@ vim.g.maplocalleader = "\\"
 
 local map = vim.keymap.set
 
-map({ "n", "v" }, "gg", "gg0", { noremap = true, silent = true, desc = "Go to start of file and first column" })
-map({ "n", "v" }, "G", "G$", { noremap = true, silent = true, desc = "Go to end of file and last column" })
-
 -- NvimTree
 map("n", "<leader>e", ":NvimTreeToggle<CR>", { desc = "Toggle file explorer" })
 
