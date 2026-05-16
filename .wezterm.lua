7aaa1ec30dde36cb1f4eff4bdbd8ec2a08d64fbf
Angel Avelar-Bonilla <angelavelarbonilla@gmail.com>
2026-05-16 00:15:26 -0400

    Update wezterm config to use different configs if on Mac instead of Windows

--- .wezterm.lua
+++ .wezterm.lua
@@ -8,17 +8,24 @@ if wezterm.config_builder then
   config = wezterm.config_builder()
 end
 
--- We kick off wezterm from Windows side
--- This file is symlinked from Windows to where it lives on WSL2 Ubuntu
-config.default_domain = 'WSL:Ubuntu-24.04'
-
+-- Settings for all
 config.font = wezterm.font('JetBrains Mono')
-config.font_size = 14
-
-config.window_decorations = "RESIZE"
-
 config.color_scheme = "Catppuccin Mocha"
-
 config.audible_bell = "Disabled"
+
+-- Helper to check for macOS
+local is_mac = wezterm.target_triple:find("apple") ~= nil
+
+if is_mac then
+    -- macOS-specific settings
+    config.font_size = 16
+else
+    -- Windows settings
+    -- We kick off wezterm from Windows side
+    -- This file is symlinked from Windows to where it lives on WSL2 Ubuntu
+    config.default_domain = 'WSL:Ubuntu-24.04'
+    config.window_decorations = "RESIZE"
+    config.font_size = 14
+end
 
 return config
